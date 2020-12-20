fd842e93380a5d2c189f706a6afe328654ca5ce6
je-l <je-l@users.noreply.github.com>
2020-12-20 22:16:28 +0200

    Implement very minimal UI with the mock data

--- client/src/toriList.tsx
+++ client/src/toriList.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import { DateTime } from "luxon";
 import { gql, useQuery } from "@apollo/client";
+
 import allToriItemsQuery from "./allToriItems.graphql";
+
+const ItemEntry = styled.div`
+  display: flex;
+  margin: 30px;
+`;
+
+const ItemDescription = styled.div`
+  margin: 10px;
+`;
+
+const ItemTitle = styled.div`
+  font-size: 18px;
+`;
+
+const ItemDetailLine = styled.div`
+  font-size: 14px;
+`;
 
 interface ToriItem {
   title: string;
+  priceEuroCents: number;
+  postedAt: string;
 }
 
 interface ToriFetchResult {
@@ -16,11 +38,17 @@ const ToriList = () => {
 
   if (!toriItems.loading) {
     const items = toriItems.data!.allToriItems.map((t) => (
-      <div key={t.title}>
+      <ItemEntry key={t.title}>
         {/* eslint-disable-next-line max-len */}
         <img src="https://images.tori.fi/api/v1/imagestori/images/1688093732.jpg?rule=adwatches" />
-        {t.title}
-      </div>
+        <ItemDescription>
+          <ItemTitle>{t.title}</ItemTitle>
+          <ItemDetailLine>{t.priceEuroCents / 100} €</ItemDetailLine>
+          <ItemDetailLine>
+            {DateTime.fromISO(t.postedAt).toLocaleString(DateTime.DATETIME_MED)}
+          </ItemDetailLine>
+        </ItemDescription>
+      </ItemEntry>
     ));
 
     return <div>{items}</div>;
