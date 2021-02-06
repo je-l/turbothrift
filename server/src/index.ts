8a7d76dd3ce48c39701ab8af2a076c274218017a
je-l <je-l@users.noreply.github.com>
2021-02-06 14:30:04 +0200

    Fail is port is missing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,13 @@
 import createServer from "./server";
 import { createDbSession } from "./database";
+import { env } from "process";
+
+const PORT = env.PORT;
+
+if (!PORT) {
+  console.error("PORT missing");
+  process.exit(1);
+}
 
 const db = createDbSession();
 const { koaApp } = createServer(db);
